2a5049076f0521e363ed27ca5c60d7d7c810fc1d
Akshay7591 <62205256+Akshay7591@users.noreply.github.com>
2021-11-21 23:17:26 +0530

    Program to get the SHA256 hash of a file
    Replace sha256 in the code with any other hashing algorithm that you want to use

---
+++ file_hash.py
@@ -0,0 +1,13 @@
+import hashlib
+
+file = "<path and name of file>" 
+BLOCK_SIZE = 65536 
+
+file_hash = hashlib.sha256() 
+with open(file, 'rb') as f:
+    fb = f.read(BLOCK_SIZE) 
+    while len(fb) > 0: 
+        file_hash.update(fb)
+        fb = f.read(BLOCK_SIZE)
+
+print (file_hash.hexdigest())
